be552f3fbf272a691ddbd5ece8f328e5ab78328b
vivek19931 <65123595+vivek19931@users.noreply.github.com>
2020-05-13 12:29:44 +0530

    Add files via upload

---
+++ ser.js
@@ -0,0 +1,41 @@
+var express=require('express'),
+app=express(),
+http=require('http'),
+socketIO=require('socket.io'),
+fs=require('fs'),
+path=require('path'),
+server,io;
+app.get('/', function(req,res){
+res.sendFile(__dirname+ '/index.html');
+});
+server=http.Server(app);
+server.Listen(8080);
+io=socketIO(server);
+io.on('connection', function(socket){
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	var readStream=fs.createReadStream(path.resolve(__dirname, './woodchunck.jpg'),{
+encoding:'binary'
+}), chunks=[];
+readStream.on('readable', function(){
+console.log('Image loading');
+});
+readStream.on('data', function(chunk){
+chunks.push(chunk);
+socket.emit('img-chunk',chunk);
+});
+readStream.on('end', function(){
+console.log('image loaded');
+});
+
+
